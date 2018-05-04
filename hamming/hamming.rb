5837295b4a584656bd943a095177e057eeddd372
Aldair_MC <ammc2023@gmail.com>
2018-05-03 23:35:29 -0400

    Hamming finish

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,4 +1,9 @@
+# Hamming, Exercism
 class Hamming
-  def compute('', '')
+  def self.compute(dna1, dna2)
+    raise ArgumentError if dna1.size != dna2.size
+    c = 0
+    dna1.size.times { |i| c += 1 if dna1.split(//)[i] != dna2.split(//)[i] }
+    c
   end
 end
