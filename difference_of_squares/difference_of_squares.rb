bc57294ae1f88c6b01d6e6c94c4a1ed401901fc8
Aldair_MC <ammc2023@gmail.com>
2018-05-12 14:51:51 -0400

    Difference of squares finish

--- difference_of_squares/difference_of_squares.rb
+++ difference_of_squares/difference_of_squares.rb
@@ -1,26 +1,30 @@
 # Difference of squares Exercism
 class Squares
-  def square_of_sum()
-    i=0
-    sum=0
-    while i < num do
+  def initialize(num)
+    @num = num
+  end
+
+  def square_of_sum
+    sum = 0
+    i = 0
+    while i <= @num
       sum += i
       i += 1
     end
     sum**2
   end
-  
-  def sum_of_squares()
-    i=0
-    sum=0
-    while i <= num do
+
+  def sum_of_squares
+    i = 0
+    sum = 0
+    while i <= @num
       sum += (i**2)
       i += 1
     end
     sum
   end
-  
-  def difference()
-    sum = newsquare_of_sum() - sum_of_squares()
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
